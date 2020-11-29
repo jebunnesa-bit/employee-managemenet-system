10a3223cf118914ae607b0da4db840740f756f33
jebunnesa-bit <68916257+jebunnesa-bit@users.noreply.github.com>
2020-11-29 19:32:47 +0600

    Update clients.php

--- views/clients/clients.php
+++ views/clients/clients.php
@@ -1,3 +1,45 @@
+<?php
+$servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "bank";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+     die("Connection failed: " . $conn->connect_error);
+    }
+
+    $sql = "UPDATE userinfo SET balance='".$amount."' WHERE userName='".$id."'";
+
+    if ($conn->query($sql) === TRUE) {
+    echo "Record updated successfully";
+    } else {
+    echo "Error updating record: " . $conn->error;
+    }
+
+    $conn->close();
+
+///view balance
+
+    $sql = "SELECT balance FROM userinfo WHERE userName='".$id."'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+    // output data of each row
+        while($row = $result->fetch_assoc()){
+            $b = $row["balance"] ;
+        }
+        $conn->close();
+    } 
+    else {
+        echo "0 results";
+        $conn->close();
+    }
+    return $b;
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
